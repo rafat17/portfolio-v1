bb9f5eb8c93c2e97b1544f9bd6253d08b297922f
rafat17 <43632236+rafat17@users.noreply.github.com>
2025-06-08 16:56:15 +0600

    fix: build issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import NavBar from "./ui/Navbar";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 // const geistSans = Geist({
